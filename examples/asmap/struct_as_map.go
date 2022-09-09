bd2343beb4ab203a0a1937887e9ab37b16c4a811
ruaabks <Aleksandr.BULGAKOV@raiffeisen.ru>
2022-09-10 01:04:26 +0300

    as-map

--- examples/asmap/struct_as_map.go
+++ examples/asmap/struct_as_map.go
@@ -7,28 +7,32 @@ type (
 )
 
 const (
-	StructFieldID              = StructField("ID")
-	StructFieldTS              = StructField("TS")
-	StructFieldName            = StructField("Name")
-	StructFieldSurname         = StructField("Surname")
-	structFieldNoExport        = StructField("noExport")
-	StructFieldNoTag           = StructField("NoTag")
-	StructFieldIgnoredInTagMap = StructField("IgnoredInTagMap")
-	StructFieldAddress         = StructField("Address")
-	StructFieldFlatNoPrefix    = StructField("FlatNoPrefix")
-	StructFieldFlatPrefix      = StructField("FlatPrefix")
+	StructFieldID                  = StructField("ID")
+	StructFieldTS                  = StructField("TS")
+	StructFieldName                = StructField("Name")
+	StructFieldSurname             = StructField("Surname")
+	structFieldNoExport            = StructField("noExport")
+	StructFieldNoTag               = StructField("NoTag")
+	StructFieldIgnoredInTagMap     = StructField("IgnoredInTagMap")
+	StructFieldAddress             = StructField("Address")
+	StructFieldFlatNoPrefixCardNum = StructField("FlatNoPrefix.CardNum")
+	StructFieldFlatNoPrefixBank    = StructField("FlatNoPrefix.Bank")
+	StructFieldFlatPrefixCardNum   = StructField("FlatPrefix.CardNum")
+	StructFieldFlatPrefixBank      = StructField("FlatPrefix.Bank")
 )
 
 func (v *Struct) AsMap() map[StructField]interface{} {
 	return map[StructField]interface{}{
-		StructFieldID:              v.ID,
-		StructFieldTS:              v.TS,
-		StructFieldName:            v.Name,
-		StructFieldSurname:         v.Surname,
-		StructFieldNoTag:           v.NoTag,
-		StructFieldIgnoredInTagMap: v.IgnoredInTagMap,
-		StructFieldAddress:         v.Address.AsMap(),
-		StructFieldFlatNoPrefix:    v.FlatNoPrefix,
-		StructFieldFlatPrefix:      v.FlatPrefix,
+		StructFieldID:                  v.ID,
+		StructFieldTS:                  v.TS,
+		StructFieldName:                v.Name,
+		StructFieldSurname:             v.Surname,
+		StructFieldNoTag:               v.NoTag,
+		StructFieldIgnoredInTagMap:     v.IgnoredInTagMap,
+		StructFieldAddress:             v.Address.AsMap(),
+		StructFieldFlatNoPrefixCardNum: v.FlatNoPrefix.CardNum,
+		StructFieldFlatNoPrefixBank:    v.FlatNoPrefix.Bank,
+		StructFieldFlatPrefixCardNum:   v.FlatPrefix.CardNum,
+		StructFieldFlatPrefixBank:      v.FlatPrefix.Bank,
 	}
 }
