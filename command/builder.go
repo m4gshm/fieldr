3c77a389eca26737f7d648f381274e8d3043f49d
Bulgakov Alexander <mfourgeneralsherman@gmail.com>
2022-09-30 19:04:33 +0300

    feat(builder): extended params

--- command/builder.go
+++ command/builder.go
@@ -20,15 +20,14 @@ func NewBuilderStruct() *Command {
 	exportVals := []string{"all", "fields", "methods"}
 
 	var (
-		flagSet       = flag.NewFlagSet(cmdName, flag.ContinueOnError)
-		name          = flagSet.String("name", generator.Autoname, "generated type name, use "+generator.Autoname+" for autoname")
-		methPrefix    = flagSet.String("method-prefix", generator.Autoname, "generated methods prefix, use "+generator.Autoname+" for autoselect")
-		valueReceiver = flagSet.Bool("value-receiver", false, "use value receiver in generate methods (default is pointer)")
-		ligth         = flagSet.Bool("ligth", false, "don't generate builder methods, only fields")
-		exports       = params.MultiValFixed(flagSet, "export", []string{}, exportVals, "export generated content")
-		// flat    = params.Flat(flagSet)
-		// snake   = params.Snake(flagSet)
-		// nolint  = params.Nolint(flagSet)
+		flagSet         = flag.NewFlagSet(cmdName, flag.ContinueOnError)
+		name            = flagSet.String("name", generator.Autoname, "generated type name, use "+generator.Autoname+" for autoname")
+		buildMethodName = flagSet.String("build-method-name", generator.Autoname, "generated build method name, use "+generator.Autoname+" for autoname")
+		setterPrefix    = flagSet.String("setter-prefix", generator.Autoname, "generated 'Set<Field>' methods prefix, use "+generator.Autoname+" for autoselect")
+		valueReceiver   = flagSet.Bool("value-receiver", false, "use value receiver in generate methods (default is pointer)")
+		ligth           = flagSet.Bool("ligth", false, "don't generate builder methods, only fields")
+		exports         = params.MultiValFixed(flagSet, "export", []string{}, exportVals, "export generated content")
+		nolint          = params.Nolint(flagSet)
 	)
 
 	return New(
@@ -67,12 +66,7 @@ func NewBuilderStruct() *Command {
 
 			btyp.SetTypeParams(btparams)
 
-			builderBody := "type " + struc.TypeString(btyp, g.OutPkg.PkgPath) + " struct {\n"
-			rec := "b"
-			constrMethodName := "Build"
-			typeParams := generator.TypeParamsString(model.Typ.TypeParams(), g.OutPkg.PkgPath)
-			constrMethodBody := "func (" + rec + " " + builderName + typeParams + ") " + constrMethodName + "() *" + buildedType + typeParams + " {\n" +
-				"return &" + buildedType + typeParams + " {\n"
+			builderBody := "type " + struc.TypeString(btyp, g.OutPkg.PkgPath) + " struct {" + generator.NoLint(*nolint) + "\n"
 
 			var exportMethods, exportFields bool
 			for _, e := range *exports {
@@ -88,10 +82,23 @@ func NewBuilderStruct() *Command {
 					return fmt.Errorf("unexpected value %s", e)
 				}
 			}
+
+			constrMethodName := "Build"
+			if len(*buildMethodName) > 0 && *buildMethodName != generator.Autoname {
+				constrMethodName = *buildMethodName
+			}
+			constrMethodName = generator.LegalIdentName(generator.IdentName(constrMethodName, exportMethods))
+			typeParams := generator.TypeParamsString(model.Typ.TypeParams(), g.OutPkg.PkgPath)
+
+			rec := "b"
+			constrMethodBody := "func (" + rec + " " + builderName + typeParams + ") " + constrMethodName + "() *" + buildedType + typeParams +
+				" {" + generator.NoLint(*nolint) + "\n"
+			constrMethodBody += "return &" + buildedType + typeParams + " {\n"
+
 			uniques := map[string]string{}
 			methodPrefix := ""
-			if len(*methPrefix) > 0 && (*methPrefix) != generator.Autoname {
-				methodPrefix = *methPrefix
+			if len(*setterPrefix) > 0 && (*setterPrefix) != generator.Autoname {
+				methodPrefix = *setterPrefix
 			}
 			if exportFields == exportMethods && methodPrefix == "" {
 				methodPrefix = defMethPref
@@ -101,7 +108,7 @@ func NewBuilderStruct() *Command {
 			if *valueReceiver {
 				typePrefix = ""
 			}
-			c, b, fmn, fmb, err := generateBuilderParts(g, model, uniques, rec, typePrefix+builderName+typeParams, methodPrefix, *ligth, exportMethods, exportFields)
+			c, b, fmn, fmb, err := generateBuilderParts(g, model, uniques, rec, typePrefix+builderName+typeParams, methodPrefix, *ligth, exportMethods, exportFields, *nolint)
 			if err != nil {
 				return err
 			}
@@ -129,7 +136,7 @@ func NewBuilderStruct() *Command {
 }
 
 func generateBuilderParts(
-	g *generator.Generator, model *struc.Model, uniques map[string]string, builderRecVar, builderType, prefix string, noMethods, exportMethods, exportFields bool,
+	g *generator.Generator, model *struc.Model, uniques map[string]string, builderRecVar, builderType, setterPrefix string, noMethods, exportMethods, exportFields, nolint bool,
 ) (string, string, []string, []string, error) {
 
 	constrMethodBody := ""
@@ -150,7 +157,7 @@ func generateBuilderParts(
 				init = "&" + init
 			}
 			constrMethodBody += fieldName + ": " + init + "{\n"
-			c, b, fmn, fmb, err := generateBuilderParts(g, fieldType.Model, uniques, builderRecVar, builderType, prefix, noMethods, exportMethods, exportFields)
+			c, b, fmn, fmb, err := generateBuilderParts(g, fieldType.Model, uniques, builderRecVar, builderType, setterPrefix, noMethods, exportMethods, exportFields, nolint)
 			if err != nil {
 				return "", "", nil, nil, err
 			}
@@ -175,10 +182,11 @@ func generateBuilderParts(
 			builderBody += builderField + " " + fullFieldType
 			constrMethodBody += fieldName + ": " + builderRecVar + "." + builderField
 			if !noMethods {
-				fieldMethodName := generator.LegalIdentName(generator.IdentName(prefix+builderField, exportMethods))
+				fieldMethodName := generator.LegalIdentName(generator.IdentName(setterPrefix+builderField, exportMethods))
 				arg := generator.LegalIdentName(generator.IdentName(builderField, false))
 
-				fieldMethod := "func (" + builderRecVar + " " + builderType + ") " + fieldMethodName + "(" + arg + " " + fullFieldType + ") " + builderType + " {\n"
+				fieldMethod := "func (" + builderRecVar + " " + builderType + ") " + fieldMethodName + "(" + arg + " " + fullFieldType + ") " + builderType +
+					" {" + generator.NoLint(nolint) + "\n"
 				fieldMethod += builderRecVar + "." + builderField + "=" + arg + "\n"
 				fieldMethod += "return " + builderRecVar + "\n}\n"
 				fieldMethodBodies = append(fieldMethodBodies, fieldMethod)
